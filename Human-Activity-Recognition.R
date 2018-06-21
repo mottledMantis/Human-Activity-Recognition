1153a0b46a3f71aca643303f99a3e33708823dbb
mottledMantis <jamesmatheson35@gmail.com>
2018-06-21 00:44:11 -0400

    Almost done, and it works!
    Just need to add mean and sd columns and run on the full data set

--- Human-Activity-Recognition.R
+++ Human-Activity-Recognition.R
@@ -19,52 +19,67 @@ y_test <- read_csv("UCI HAR Dataset/test/y_test.txt", col_names = FALSE)
 # View(X_test)
 # View(y_test)
 
-colnames(y_train) <- ("labels")
+colnames(y_train) <- ("ActivityLabel")
 colnames(x_train) <- ("data1")
-colnames(y_test) <- ("labels")
+colnames(y_test) <- ("ActivityLabel")
 colnames(x_test) <- ("data1")
 colnames(subject_test) <- ("subject")
 colnames(subject_train) <- ("subject")
 
-# train <- full_join(y_train, x_train)
-# test <- full_join(y_test, x_test)
-# View(train)
-# View(test)
-train <- bind_cols(subject_train["subject"], y_train["labels"], x_train["data1"])
-test <- bind_cols(subject_test["subject"], y_test["labels"], x_test["data1"])
-# View(train)
-# View(test)
+#create dfs for train and test sets and merge them into all_data
+train <- bind_cols(subject_train["subject"], y_train["ActivityLabel"], x_train["data1"])
+test <- bind_cols(subject_test["subject"], y_test["ActivityLabel"], x_test["data1"])
 all_data <- bind_rows(train, test)
+# View(train)
+# View(test)
 # View(all_data)
 
 
-new_feat <- data.frame(matrix(ncol = (nrow(features)), nrow = 0))
-
-colnames(new_feat) <- features$X2
+new_feat <- as.tbl(data.frame(matrix(ncol = (nrow(features)), nrow = 0)))
 
 
+#if I name the columns here, they work initially, but once I add rows below these names
+#are overwritten with Xn and the values of row 1 - WHY???
+# colnames(new_feat) <- features$X2
+
+#initialize i for while loop - with a version for the full set and a safe version
 i <- 1
-while (i < nrow(all_data)) {
-# print(i)
-# vectorize a data cell
-cell <- c(unlist(strsplit(as.character(all_data[i, 3]), split = "\\s+")))
 
-# pass the vector as a new row using rbind()
-new_feat <- rbind(new_feat, cell, stringsAsFactors = FALSE)
-i <- i + 1
+#while (i < nrow(all_data)) {
+while (i < 50) {
+
+# vectorize a data cell
+  cell <- c(unlist(strsplit(as.character(all_data[i, 3]), split = "\\s+")))
+
+# pass the vector to a new row using rbind()
+  new_feat <- rbind(new_feat, cell, stringsAsFactors = FALSE)
+  i <- i + 1
 }
 
-# # strsplit(as.character(all_data[2, "data1"]), split = "\\s+")
-# i <- 1 #outer loop, col
-# j <- 1 #inner loop, row
-# while (i < nrow(all_data)) {
-#   print(i)
-#   while (j < nrow(features)) {
-#     print(j)
-#     new_feat[1,1] <- unlist(strsplit(as.character(all_data[j, 3]), split = "\\s+"))
-#   j <- j + 1
-#   }
-# i <- i + 1
-# }
+#why does this only stick is placed AFTER adding rows?
+colnames(new_feat) <- features$X2
 
+j <- 1
 
+while (j < nrow(all_data)) {
+  if (all_data[j, "ActivityLabel"] == 1) {
+    all_data[j, "ActivityName"] <- sub("^..", "", activity_labels[1,1])
+  }
+  else if (all_data[j, "ActivityLabel"] == 2) {
+    all_data[j, "ActivityName"] <- sub("^..", "", activity_labels[2,1])
+  }
+  else if (all_data[j, "ActivityLabel"] == 3) {
+    all_data[j, "ActivityName"] <- sub("^..", "", activity_labels[3,1])
+  }
+  else if (all_data[j, "ActivityLabel"] == 4) {
+    all_data[j, "ActivityName"] <- sub("^..", "", activity_labels[4,1])
+  }
+  else if (all_data[j, "ActivityLabel"] == 5) {
+    all_data[j, "ActivityName"] <- sub("^..", "", activity_labels[5,1])
+  }
+  else if (all_data[j, "ActivityLabel"] == 6) {
+    all_data[j, "ActivityName"] <- sub("^..", "", activity_labels[6,1])
+  }
+j <- j + 1
+}
+print(sub("^..", "", "1 ghd"))
